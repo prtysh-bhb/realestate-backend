957a01da261007df8ce84870f4531ee1cff24e13
Pratyush <pratyushs.brainerhub@gmail.com>
2025-11-11 17:10:47 +0530

    cors fixes

--- config/cors.php
+++ config/cors.php
@@ -5,15 +5,20 @@ return [
     
     'allowed_methods' => ['*'],
     
-    'allowed_origins' => ['http://127.0.0.1:5173', 'http://localhost:5173'],
+    'allowed_origins' => array_filter(
+        array_map('trim', explode(',', env('ALLOWED_ORIGINS', '*')))
+    ),
     
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        'http://localhost:*',
+        'http://127.0.0.1:*',
+    ],
     
     'allowed_headers' => ['*'],
     
-    'exposed_headers' => [],
+    'exposed_headers' => ['*'],
     
     'max_age' => 0,
     
-    'supports_credentials' => true,
+    'supports_credentials' => env('CORS_SUPPORTS_CREDENTIALS', true),
 ];
