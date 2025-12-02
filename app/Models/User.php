1e43f01c0806e83ab81aa990ee089e6456029788
rohitmbrainerhub <rohitm.brainerhub@gmail.com>
2025-12-02 17:22:56 +0530

    changes: user module

--- app/Models/User.php
+++ app/Models/User.php
@@ -132,37 +132,4 @@ class User extends Authenticatable
     {
         return $this->hasMany(AgentReview::class, 'user_id');
     }
-
-    public function subscriptions()
-    {
-        return $this->hasMany(UserSubscription::class);
-    }
-
-    public function activeSubscription()
-    {
-        return $this->hasOne(UserSubscription::class)
-            ->where('status', 'active')
-            ->where('ends_at', '>', now())
-            ->latest();
-    }
-
-    public function payments()
-    {
-        return $this->hasMany(Payment::class);
-    }
-
-    public function agentAppointments()
-    {
-        return $this->hasMany(Appointment::class, 'agent_id');
-    }
-
-    public function customerAppointments()
-    {
-        return $this->hasMany(Appointment::class, 'customer_id');
-    }
-
-    public function reminders()
-    {
-        return $this->hasMany(Reminder::class, 'agent_id');
-    }
 }
